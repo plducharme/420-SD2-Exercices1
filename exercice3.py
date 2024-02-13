08040095e7f85535637c02e96ee17e8ac8722d36
Pier Luc Ducharme <dutch80@gmail.com>
2024-02-13 16:18:58 -0500

    modification question 3

--- exercice3.py
+++ exercice3.py
@@ -1,2 +1,2 @@
-# Quelle serait la déclaration d’une liste en compréhension utilisant un lambda pour initialiser une liste contenant
+# Quelle serait la déclaration d’une liste en compréhension pour initialiser une liste contenant
 # les 20 premiers éléments (« range(20) ») du restant de la division par 10 représentés sous la forme de string?
